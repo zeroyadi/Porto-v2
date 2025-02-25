38360eda475f177380b8bca29b57a4016a207ca4
zeroyadi <zeroyadi024@gmail.com>
2025-02-25 07:28:17 +0700

    frame-Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,9 +48,9 @@ const Hero = () => {
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
             <motion.img
-              initial= {{ x: 100, opacity: 0 }}
-              animate={{ x: 0, opacity: 1}}
-              transition={{ duration: 1, delay: 1.2}}
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
               src={Galaxy}
               alt="Galaxy"
             />

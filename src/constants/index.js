7873701d7ef8395d8d4c280013c1fb7e2b3c1b91
zeroyadi <zeroyadi024@gmail.com>
2025-03-14 23:39:55 +0700

    ngubah project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,5 @@
-import project1 from "../assets/aot.png";
-import project2 from "../assets/Dark F.jpg";
-import project3 from "../assets/date.jpg";
-import project4 from "../assets/eren GF.gif";
+import project1 from "../assets/Date a Live.png";
+import project2 from "../assets/Deskify.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -40,33 +38,19 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Website Streaming",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS"],
   },
   {
-    title: "Task Management App",
+    title: "Deskify",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+      "Bangun Laptop Terbaik Anda Pengaturan dengan Mudah. Website yang menyediakan set up meja/laptop kalian.",
+    technologies: ["React.js", "CSS", "Tailwind.css", "Firebase"],
+  }
 ];
 
 export const CONTACT = {
